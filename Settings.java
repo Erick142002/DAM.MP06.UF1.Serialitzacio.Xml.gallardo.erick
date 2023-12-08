06a67148e2735004ad970bdb2b45386fdd80275f
Erick <132074706+Erick142002@users.noreply.github.com>
2023-12-08 18:43:58 +0100

    Settings.java

--- Settings.java
+++ Settings.java
@@ -2,7 +2,7 @@ package gallardo.erick.dam.mp06.uf01.p03.serialitzacio.xml.model.configuration;
 
 public class Settings {
 	
-	private static final String CAR_FOLDER = "ruta/del/teu/directori/de/coches/";
+	private static final String CAR_FOLDER = "C:\Users\erick\OneDrive\Desktop\DAM1\DAM.MP06.UF01.P03.Serialitzacio.Xml\src";
     public static final String FITXER_COTXE = CAR_FOLDER + "cotxe.xml";
 
 	public static final String FITXER_COTXE_XML = null;
